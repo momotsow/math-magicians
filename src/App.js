7a81bf5540c82007b01b355772c6ba2f629c51c2
momotsow <kgomotsomkansi@gmail.com>
2022-11-30 13:58:35 +0200

    Add header and router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import react from 'react';
 import Calculator from './components/calculator';
 import './App.css';
+import Home from './components/home';
+import Header from './components/header';
 
-class App extends react.Component {
-  render() {
-    return <Calculator />;
-  }
-}
+const App = () => (
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/Quote" element={<Quote />} />
+    </Routes>
+  </>
+);
 
 export default App;

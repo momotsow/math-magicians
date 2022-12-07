7cd901474893008c2512b6b2b506e891c82b46d4
momotsow <kgomotsomkansi@gmail.com>
2022-12-07 20:08:37 +0200

    Add home and quote page text

--- src/components/quote.js
+++ src/components/quote.js
@@ -5,12 +5,12 @@ const Home = () => (
   <div className="homeContainer">
     <div className="content">
       <div className="contentTitle">
-        <h2> Quotes  </h2>
+        <h2> Quotes</h2>
       </div>
       <div className="contentDescription">
         <p>
           Mathematics is not about numbers, equations, computations,
-          or algorithms: it is about understanding. -William Paul thurston.
+          or algorithms: it is about understanding. - William Paul thurston.
           <br />
           <br />
         </p>

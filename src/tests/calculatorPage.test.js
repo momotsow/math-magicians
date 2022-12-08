513e0a1acda60d972efffce772faf8be17e312f3
momotsow <kgomotsomkansi@gmail.com>
2022-12-09 01:05:43 +0200

    Add calculator page unit test

---
+++ src/tests/calculatorPage.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Calculate from '../components/Calculator';
+
+test('renders Calculate component', () => {
+  const { asFragment } = render(<Calculate />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('User can click on the buttons', () => {
+  const handleClick = jest.fn();
+  render(<button type="button" onClick={handleClick} className="input-button">AC</button>);
+  const button = screen.getByText('AC');
+  userEvent.click(button);
+  expect(handleClick).toHaveBeenCalled();
+});

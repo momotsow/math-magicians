254c5a920ac025cc20d311ae7d354995ec61fcfe
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-12-08 22:41:43 +0100

    test: add home page tests

---
+++ src/tests/home.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Home from '../components/home';
+
+describe('Testing home page to render as expected', () => {
+const homePage = render(
+      <Router>
+        <Home />
+      </Router>,
+    );
+  it('Home should render correctly', () => {
+    //Assert
+    expect(homePage).toMatchSnapshot();
+
+  });
+
+  it('Home should contains the title Welcome to home page', () => {
+    //Arrange
+    const title = screen.getByText('Welcome to home page');
+    //Assert
+    expect(title).toBeInTheDocument();
+  }
+  
+});

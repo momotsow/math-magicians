d567120f2024969acf7befefd3166fdb52268715
momotsow <kgomotsomkansi@gmail.com>
2022-12-08 21:52:57 +0200

    Required changes

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -5,8 +5,8 @@ describe('Testing operate funtion', () => {
     expect(operate(6, 9, '+')).toEqual('15');
   });
 
-  test('Test operate Logic (Subtract) | operate(5, -2, -)', () => {
-    expect(operate(5, -2, '-')).toEqual('7');
+  test('Test operate Logic (Subtract) | operate(5, 2, -)', () => {
+    expect(operate(5, 2, '-')).toEqual('3');
   });
 
   test('Test operate Logic (Multiply) | operate(4, 18, x)', () => {
